10c738afdca9a818005d21a3ff06bf572c94b841
naseersyed56 <naseersyed560@gmail.com>
2026-05-29 18:46:23 +0530

    Time: 2 ms (50.81%), Space: 8.6 MB (37.6%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        int rev = 0;
-        if(x< 0 || (x%10==0 && x!=0)) return false;
-        while(x>rev){
-            rev = rev*10 + x % 10;
-            x/=10;
-        }
-     return(x == rev || x ==rev/10);  
-       
+      
+     long long rev =0;
+
+      if( x<0 ||(x%10==0 && x!=0 ))return false;
+
+      for(int j =x; j!=0 ;j/=10){
+          
+          rev =rev * 10+j%10;
+      }
+       return( x == rev);
+         
+
     }
 };
