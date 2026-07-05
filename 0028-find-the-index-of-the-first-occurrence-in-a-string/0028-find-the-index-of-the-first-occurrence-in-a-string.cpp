a01b5bd4c5833405ecd087d2c6419de5bb8a6cd6
naseersyed56 <naseersyed560@gmail.com>
2026-07-05 19:01:25 +0530

    Time: 0 ms (100%), Space: 9.2 MB (26.33%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -4,24 +4,21 @@ public:
         if(needle.size()>haystack.size())return-1;
 
         for(int i=0 ;i<=haystack.size()-needle.size();i++){
-            
-          int count=0;
-            
+            int count=0;
 
-            for(int j=0 ;j<=needle.size()-1;j++){
+            for(int j=0;j<needle.size();j++){
 
-                if(haystack[i+j]==needle[j]){
-                    count++;
-                 if(  count==needle.size())
-                    return i;
-                 }else{
+                if(haystack[i+j]!=needle[j]){
                     break;
-                 }
-             }
-            }
-        
-     return -1;
+                }else{
+                    count++;
+                    if(count ==needle.size())return i;
 
-        
+                }
+            }
+
+        }
+
+        return -1;
     }
 };
