f3849617305a4adb924faff7d7192759119e4a4a
naseersyed56 <naseersyed560@gmail.com>
2026-07-08 23:46:30 +0530

    Time: 1 ms (71.22%), Space: 9 MB (43.06%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -4,12 +4,11 @@ public:
         
         if(n<=0)return false;
         if(n==1)return true;
-        while(n>1){
-            if(n%3!=0){
-                return false;
-            }
-            n/=3;
+      while(n>1){
+        
+            if(n%3!= 0)return false;
+            else n=n/3;
         }
         return true;
-    }
-};
+      }
+    };
