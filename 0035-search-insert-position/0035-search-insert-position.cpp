24d9caa8159bfd0f8922e4b07470873ed24d42f4
naseersyed56 <naseersyed560@gmail.com>
2026-07-15 14:57:00 +0530

    Time: 0 ms (100%), Space: 13.7 MB (43.19%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -4,21 +4,20 @@ public:
 
         int start =0 ,end =nums.size()-1;
 
-        while(start<=end){
+       while(start<=end){
+        
+        int mid = start+ (end-start)/2;
 
-            int mid = start +(end-start)/2;
+        if(nums[mid]==target)return mid;
+        
+        else if( nums[mid]>target)end=mid-1;
 
-            if(nums[mid]==target){
-                return mid;
-            }
-            else if(nums[mid]<target){
-                start =mid+1;
-            }else
-            {
-                end=mid-1;
-            }
+        else{
+
+            start =mid+1;
         }
-        return start;
+       }
+       return start;
         
     }
 };
