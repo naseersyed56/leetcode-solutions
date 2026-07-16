f367e5bdfd9350585d6099b927e0584cbcf5ded4
naseersyed56 <naseersyed560@gmail.com>
2026-07-16 18:33:43 +0530

    Time: 13 ms (14.05%), Space: 12.3 MB (67.1%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -3,23 +3,22 @@ public:
     int romanToInt(string s) {
        int sum =0;
 
-       unordered_map<char ,int> k;
-       k['M'] =1000;
-       k['D'] =500;
-       k['C'] =100;
-       k['L'] =50;
-       k['X'] =10;
-       k['V'] =5;
-       k['I']=1;
+       unordered_map<char,int>L;
+
+       L['M']=1000;
+       L['D']=500;
+       L['C']=100;
+       L['L']=50;
+       L['X']=10;
+       L['V']=5;
+       L['I']=1;
 
        for(int i=0 ;i<s.size();i++){
-        if(i<s.size()-1){
-        if(k[s[i]]<k[s[i+1]])sum-=k[s[i]];
-        else sum +=k[s[i]];
-       }
-       else{
-        sum+=k[s[i]];
-       }
+
+        if( i<s.size()-1){
+            if(L[s[i]]<L[s[i+1]])sum-=L[s[i]];
+        else sum+=L[s[i]];
+       }else sum+=L[s[i]];
     }
     return sum;
     }       
