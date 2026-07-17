21b79cbfcbaa97d3c171f290d29820182a45b60b
naseersyed56 <naseersyed560@gmail.com>
2026-07-17 10:49:53 +0530

    Time: 0 ms (100%), Space: 7.7 MB (54.02%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.cpp
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -5,26 +5,23 @@ public:
        // int k=num%10;
         //if(k ==2|| k==3|| k==7 ||k==8 )return false;
         //else{
-           int start =0;
-           long long end =num;
-            while(start<=end){
+           int start =0 , end =num;
 
-               long long mid =start+( end-start)/2;
+           while(start<=end){
 
-               long long square =mid*mid;
+            long long  mid =start +(end-start)/2;
 
-                if(square ==num)return true;
+            long long sqr =mid *mid;
 
+            if(sqr ==num)return true;
 
-                if(mid * mid >num)end=mid-1;
-                else{
-                    start=mid+1;
-                }
-            }
-
-           //}
+           
+            else if(sqr> num) end =mid-1;
+            else
+                start = mid+1;
+                                
+           }
            return false;
-
            
         
     }
