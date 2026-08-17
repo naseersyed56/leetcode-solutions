0a6f2b41a22176b7784c7e9074317ad11c88524e
naseersyed56 <naseersyed560@gmail.com>
2026-08-17 22:23:51 +0530

    Time: 0 ms (100%), Space: 11.7 MB (27.06%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -3,18 +3,17 @@ public:
     vector<int> plusOne(vector<int>& digits) {
 
 
-        for(int i=digits.size()-1 ;i>-1;i--){
+        for(int i=digits.size()-1 ; i>=0;i--){
 
-            if(digits[i]!= 9){
-
+            if(digits[i] !=9){
                 digits[i]++;
                 return digits;
             }
-
-            digits[i]=0;
+            
+           else  digits[i]=0;
         }
 
-        digits.insert(digits.begin(),1);
+        digits.insert(digits.begin() ,1);
 
         
         return digits;
